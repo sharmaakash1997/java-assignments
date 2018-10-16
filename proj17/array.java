869e66d1bef203e8a363961d750b58ee2fd00c18
Akash <jamdagniakash@gmail.com>
2018-10-16 12:22:48 +0530

    Assign

---
+++ proj17/array.java
@@ -0,0 +1,24 @@
+class MainClass 
+{
+  public static <T> void printArray(T[] inputArray) 
+  {
+    for (T element : inputArray)
+    System.out.printf("%s ", element);
+
+            System.out.println();
+  }
+
+         public static void main(String args[]) 
+  {
+        Integer[] integerArray = { 1, 2, 3, 4, 5, 6 };
+    Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+    Character[] characterArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            System.out.println(" Array integer Array contains:");
+    printArray(integerArray); 
+    System.out.println("\n Array double Array contains:");
+    printArray(doubleArray);
+    System.out.println("\n Array character Array contains:");
+    printArray(characterArray);
+  } 
+}
